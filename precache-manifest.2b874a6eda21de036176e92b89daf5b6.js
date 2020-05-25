0095ec08bfbcd6c3faa33a71928579c1b3a92830
thip <thipsavanh@gmail.com>
2020-05-24 23:33:37 -0500

    Updates

---
+++ precache-manifest.2b874a6eda21de036176e92b89daf5b6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0a5dd3b96880206558130845f602be",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "969911433e7c96805039",
+    "url": "/React_Portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "66d20dc0de5c05f65105",
+    "url": "/React_Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "969911433e7c96805039",
+    "url": "/React_Portfolio/static/js/2.c18d5f27.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Portfolio/static/js/2.c18d5f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d20dc0de5c05f65105",
+    "url": "/React_Portfolio/static/js/main.bda18042.chunk.js"
+  },
+  {
+    "revision": "c083a7718907dd4b36b5",
+    "url": "/React_Portfolio/static/js/runtime-main.73c16ebd.js"
+  },
+  {
+    "revision": "624829d4335d4491830ae34a65308da8",
+    "url": "/React_Portfolio/static/media/bookmart.624829d4.png"
+  },
+  {
+    "revision": "4f1cb58dc107929868cbc2722ea2ce7a",
+    "url": "/React_Portfolio/static/media/ghostapp.4f1cb58d.png"
+  },
+  {
+    "revision": "a4afce403a8e788a670bf15f5e5306ff",
+    "url": "/React_Portfolio/static/media/password.a4afce40.png"
+  },
+  {
+    "revision": "3ffc1cf7430c520073893e7cb6ade894",
+    "url": "/React_Portfolio/static/media/quiz.3ffc1cf7.png"
+  }
+]);
