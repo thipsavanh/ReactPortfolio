9f09c0d923b074f3835cccde217f3c35104261fc
thip <thipsavanh@gmail.com>
2020-05-25 13:16:24 -0500

    Updates

---
+++ precache-manifest.658b86bfce0ca260c801ff61abbc9f53.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b77037cfe02efe00f1833f3a4bfd99f",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "189c5553f106315319d4",
+    "url": "/React_Portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a310f8795c45d0dd9224",
+    "url": "/React_Portfolio/static/css/main.8490083b.chunk.css"
+  },
+  {
+    "revision": "189c5553f106315319d4",
+    "url": "/React_Portfolio/static/js/2.282bccdf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Portfolio/static/js/2.282bccdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a310f8795c45d0dd9224",
+    "url": "/React_Portfolio/static/js/main.8bc4b75b.chunk.js"
+  },
+  {
+    "revision": "c083a7718907dd4b36b5",
+    "url": "/React_Portfolio/static/js/runtime-main.73c16ebd.js"
+  },
+  {
+    "revision": "2d47e6a48ecc7fd44caa069c4c825c6c",
+    "url": "/React_Portfolio/static/media/Resume.2d47e6a4.pdf"
+  },
+  {
+    "revision": "a5d56950526e10079521a940ea9ec2f4",
+    "url": "/React_Portfolio/static/media/biophoto.a5d56950.jpg"
+  },
+  {
+    "revision": "624829d4335d4491830ae34a65308da8",
+    "url": "/React_Portfolio/static/media/bookmart.624829d4.png"
+  },
+  {
+    "revision": "713ad88ac1aff7e8e460472e6be5753a",
+    "url": "/React_Portfolio/static/media/fitnesstracker.713ad88a.png"
+  },
+  {
+    "revision": "4f1cb58dc107929868cbc2722ea2ce7a",
+    "url": "/React_Portfolio/static/media/ghostapp.4f1cb58d.png"
+  },
+  {
+    "revision": "ea68e02635a4c0baaf300a2d50eed5a2",
+    "url": "/React_Portfolio/static/media/notetaker.ea68e026.png"
+  },
+  {
+    "revision": "a4afce403a8e788a670bf15f5e5306ff",
+    "url": "/React_Portfolio/static/media/password.a4afce40.png"
+  },
+  {
+    "revision": "3ffc1cf7430c520073893e7cb6ade894",
+    "url": "/React_Portfolio/static/media/quiz.3ffc1cf7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React_Portfolio/precache-manifest.eca229379f8a83082f0137f49e4fcf5f.js"
+  "/React_Portfolio/precache-manifest.658b86bfce0ca260c801ff61abbc9f53.js"
 );
 
 self.addEventListener('message', (event) => {
