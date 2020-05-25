083bb7735aa3a4db92486038a67339f367d12ece
thip <thipsavanh@gmail.com>
2020-05-24 23:18:40 -0500

    created foundation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,29 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom'; 
-import About from './components/About.js'; 
-import Portfolio from './components/Portfolio.js'; 
-import Contact from './components/Contact.js';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Thipsavanh Douangboupha",
+      headerLinks: [
+        { title: "About", path: "/" },
+        { title: "Porfolio", path: "/portfolio" },
+        { title: "Contact", path: "/contact" }
+      ],
+      about: {
+        title: "About Me"
+      },
+      portfolio: {
+        title: "Here are some of my projects: "
+      },
+      contact: {
+        title: "I'd love to hear from you"
+      }
+    }
+  }
+
+
+render() {
   return (
-    <BrowserRouter>
-    <div className="App">
-        <Route exact path="/" component={About} />
-        <Route path="/Portfolio" component={Portfolio} />
-        <Route path="/Contact" component={Contact} />
-        <div className="navigation">
-          <img src={logo} className="logo-image" alt="Logo Image" />
-          <div className="navigation-sub">
-            <Link to="/" className="item">About</Link>
-            <Link to="/Portfolio" className="item">Portfolio</Link>
-            <Link to="/Contact" className="item">Contact</Link>
-          </div>
-        </div>
-    </div>
-    </BrowserRouter>
+    <Router>
+      <Container className="p-0" fluid={true}>
+        <Navbar className="border-bottom" bg="transparent" expand="lg">
+          <Navbar.Brand>Thipsavanh Douangboupha</Navbar.Brand>
+          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+              <Link className="nav-link" to="/">About</Link>
+              <Link className="nav-link" to="portfolio">Portfolio</Link>
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Route path="/" exact render={() => <About title={this.state.about.title} />} />
+        <Route path="/portfolio" render={() => <Portfolio title={this.state.portfolio.title} />} />
+        <Route path="/contact" render={() => <Contact title={this.state.contact.title} />} />
+        <Footer />
+      </Container>
+    </Router>
   );
+}
 }
 
 export default App;
